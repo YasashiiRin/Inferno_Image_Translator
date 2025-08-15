b54e62cadbc664b75f39cc28a4e9acd15417b8b6
tuannv <tuannv@d-soft.com.vn>
2025-08-15 17:55:46 +0700

    create coordinates for images

--- background.js
+++ background.js
@@ -14,27 +14,57 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     });
   }
 });
+let translationQueue = [];
+let isProcessing = false;
+
+async function fetchTranslation(imageUrl) {
+  try {
+    const response = await fetch("http://192.168.4.194:5000/translate-image", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({imageUrl})
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("=====> data response", data)
+    return {success: true, results: data};
+  } catch (error) {
+    console.error("=====> Error when call ORC:", error);
+    return {success: false, error: error.toString()};
+  }
+}
+
+async function processQueue() {
+  console.log("=====> call processQueue")
+  if (isProcessing || translationQueue.length === 0) return;
+  isProcessing = true;
+
+  const {imageUrl, callback} = translationQueue.shift();
+
+  try {
+    const data = await fetchTranslation(imageUrl);
+    callback(data);
+  } catch (error) {
+    console.error("=====> Error when process translation:", error);
+    callback({success: false, error: error.toString()});
+  } finally {
+    isProcessing = false;
+    processQueue();
+  }
+  
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("=====> call fetch")
   if (message.action === "fetchTranslation") {
-    fetch("http://192.168.4.191:5000/translate-image", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ imageUrl: message.imageUrl })
-    })
-      .then(res => {
-        if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
-        return res.json();
-      })
-      .then(data => {
-        console.log("Fetch response:", data);
-        sendResponse(data); // Luôn gọi sendResponse
-      })
-      .catch(err => {
-        console.error("Fetch error in background:", err);
-        sendResponse({ success: false, error: err.toString() }); // Trả về lỗi
-      });
+    translationQueue.push({imageUrl: message.imageUrl, callback: sendResponse});
+    console.log(`Đã thêm ${message.imageUrl} vào queue. Queue length: ${translationQueue.length}`);
+    processQueue();
 
-    return true; // Giữ connection cho async
+    return true;
   }
 });
